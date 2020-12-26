29dacb70d99fe4c2c5c62733d16d5cc99c716811
Ravikant Khandelwal <ravikant.khandelwal@bts.com>
2020-12-26 22:56:37 +0530

    Updates

---
+++ precache-manifest.7827498c21086fe7dd2ff0b4ab998d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101d96034bb427d8456f9eb20453f001",
+    "url": "/react-tours/index.html"
+  },
+  {
+    "revision": "40690758dd16d399ff64",
+    "url": "/react-tours/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react-tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40690758dd16d399ff64",
+    "url": "/react-tours/static/js/main.22f65c5c.chunk.js"
+  },
+  {
+    "revision": "3361ebb3c243618fc7ca",
+    "url": "/react-tours/static/js/runtime-main.8e9505e0.js"
+  }
+]);
